b68ca9301e845c879dac331b61dfea2e32de7693
Tatiaynti <tatianamo1234@mail.ru>
2021-05-30 17:12:48 +0300

    Исправлены ошибки

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@
 const getRandomNumber = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
-return (max > min && min >= 0)
-? Math.floor(Math.random() * (max - min + 1)) + min
-: 'Задан некорректный диапазон';
+  return (max > min && min >= 0)
+    ? Math.floor(Math.random() * (max - min + 1)) + min
+    : 'Задан некорректный диапазон';
 };
+getRandomNumber(2, 342);
 
 // Используемые источники
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -15,9 +16,10 @@ return (max > min && min >= 0)
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 
 const getRandomFloat = (min, max, decimalPoints) => {
-  if (max > min && min >= 0 && decimalPoints >= 0) {
-   const randomNumber = Math.random() * (max - min + 1) + min;
-   return randomNumber.toFixed(decimalPoints);
+    if (max > min && min >= 0 && decimalPoints >= 0) {
+  const randomNumber = Math.random() * (max - min + 1) + min;
+    return randomNumber.toFixed(decimalPoints);
   }
-  return 'Заданы некорректные координаты';
+return 'Заданы некорректные координаты';
 }
+getRandomFloat(2, 12, 3);
