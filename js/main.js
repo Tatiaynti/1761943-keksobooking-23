e03a6c931c3f067b75eb003bdb798d3ea10339cc
Tatiaynti <tatianamo1234@mail.ru>
2021-05-30 16:44:51 +0300

    Выполнено задание "нужно больше функций"
    Подключен файл js, добавлены две функции на вычисление целого числа и числа с плавающей точкой из заданного диапазона.

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно.
+
+const getRandomNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+return (max > min && min >= 0)
+? Math.floor(Math.random() * (max - min + 1)) + min
+: 'Задан некорректный диапазон';
+};
+
+// Используемые источники
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// https://learn.javascript.ru/number
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+
+const getRandomFloat = (min, max, decimalPoints) => {
+  if (max > min && min >= 0 && decimalPoints >= 0) {
+   const randomNumber = Math.random() * (max - min + 1) + min;
+   return randomNumber.toFixed(decimalPoints);
+  }
+  return 'Заданы некорректные координаты';
+}
