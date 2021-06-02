e53bdd1e8d04002434b6f3c38a3daf46a9997075
Tatiaynti <tatianamo1234@mail.ru>
2021-06-02 22:58:08 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,25 +1,22 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно.
-
 const getRandomNumber = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return (max > min && min >= 0)
     ? Math.floor(Math.random() * (max - min + 1)) + min
-    : 'Задан некорректный диапазон';
+    : undefined;
 };
 getRandomNumber(2, 342);
-
-// Используемые источники
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-// https://learn.javascript.ru/number
-
-// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-
+/**
+ * Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+ * @param {*} min - начальное значение диапазона (от)
+ * @param {*} max - конечное значение диапазона (до)
+ * @param {*} decimalPoints - кол-во знаков после запятой
+ */
 const getRandomFloat = (min, max, decimalPoints) => {
   if (max > min && min >= 0 && decimalPoints >= 0) {
     const randomNumber = Math.random() * (max - min + 1) + min;
     return randomNumber.toFixed(decimalPoints);
   }
-  return 'Заданы некорректные координаты';
+  return undefined;
 };
 getRandomFloat(2, 12, 3);
