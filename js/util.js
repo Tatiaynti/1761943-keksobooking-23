ce29450d4481b0e2cc8010b169fb09a657a51bd9
Tatiaynti <tatianamo1234@mail.ru>
2021-06-16 22:41:04 +0300

    Разделение на модули
    Разделен на модули существующий код, добавлены пустые модули, которые потребуются в дальнейшем.

---
+++ js/util.js
@@ -0,0 +1,20 @@
+const getRandomNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return (max > min && min >= 0)
+    ? Math.floor(Math.random() * (max - min + 1)) + min
+    : undefined;
+};
+
+const getRandomFloat = (min, max, decimalPoints = 1) => {
+  if (max > min && min >= 0) {
+    const randomNumber = Math.random() * (max - min + 1) + min;
+    return randomNumber.toFixed(decimalPoints);
+  }
+  return undefined;
+};
+
+const getRandomArrayElement = (elements) =>
+  elements[getRandomNumber(0, elements.length - 1)];
+
+  export {getRandomNumber, getRandomFloat, getRandomArrayElement};
