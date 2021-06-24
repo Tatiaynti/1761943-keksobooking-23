52d6a6ed195cea797685b1cd9d668bd8b88ffa41
Tatiaynti <tatianamo1234@mail.ru>
2021-06-24 20:21:36 +0300

    правки npm tests

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,3 @@
-
 const similarOfferTemplate = document.querySelector('#card').content;
 const offerType = {
   'flat': 'Квартира',
@@ -24,31 +23,31 @@ const fillOfferTemplate = (author, offer) => {
     if (offer.photos.length === 0) {offerElement.querySelector('.popup__photos').remove();}
   };
 
-    offerElement.querySelector('.popup__title').textContent = offer.title;
-    offerElement.querySelector('.popup__text--address').textContent = offer.address;
-    offerElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
-    offerElement.querySelector('.popup__type').textContent = offerType[offer.type];
-    offerElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-    offerElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-    offerElement.querySelector('.popup__description').textContent = offer.description;
-    offerElement.querySelector('.popup__avatar').src = author;
+  offerElement.querySelector('.popup__title').textContent = offer.title;
+  offerElement.querySelector('.popup__text--address').textContent = offer.address;
+  offerElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
+  offerElement.querySelector('.popup__type').textContent = offerType[offer.type];
+  offerElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  offerElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+  offerElement.querySelector('.popup__description').textContent = offer.description;
+  offerElement.querySelector('.popup__avatar').src = author;
 
-    const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
-    const featuresOffer = offerElement.querySelectorAll('.popup__feature');
-    featuresOffer.forEach((item) => {
-      const modifier = item.classList[1];
-      if (!modifiers.includes(modifier)) {
-        item.remove();
-      }
-    });
+  const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
+  const featuresOffer = offerElement.querySelectorAll('.popup__feature');
+  featuresOffer.forEach((item) => {
+    const modifier = item.classList[1];
+    if (!modifiers.includes(modifier)) {
+      item.remove();
+    }
+  });
 
-    const offerPhotos = offerElement.querySelector('.popup__photos');
-    offer.photos.find((photoSrc) => {
-      const offerPhoto = offerPhotos.querySelector('.popup__photo').cloneNode(false);
-      offerPhoto.src = photoSrc;
-      offerPhotos.appendChild(offerPhoto);
-    });
-    offerPhotos.querySelector('.popup__photo').remove();
+  const offerPhotos = offerElement.querySelector('.popup__photos');
+  offer.photos.find((photoSrc) => {
+    const offerPhoto = offerPhotos.querySelector('.popup__photo').cloneNode(false);
+    offerPhoto.src = photoSrc;
+    offerPhotos.appendChild(offerPhoto);
+  });
+  offerPhotos.querySelector('.popup__photo').remove();
 
   checkAvailability();
   return offerElement;
